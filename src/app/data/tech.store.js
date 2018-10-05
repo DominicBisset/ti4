4e227d211c3a5276c71624b0ebf84aae51a33fe6
Dominic Bisset <dominicbisset@gmail.com>
2018-10-05 11:17:35 +0100

    Added order to tech list

--- src/app/data/tech.store.js
+++ src/app/data/tech.store.js
@@ -6,36 +6,13 @@ class TechStore extends Store{
         let response = await fetch('./data/technologies.json');
         let data = await response.json();
         console.log("loaded techs", data.techs);
-        this.set({ techs: data.techs });
-        // let d = document;
-        // let list = d.getElementById("techlist");
-        // for(let key in data.techs){
-        //     let tech = data.techs[key];
-        //     console.log(tech)
-        //     let div = d.createElement("div");
-        //     div.setAttribute("class","techitem");
-        //     let h3 = d.createElement("h3");
-        //     h3.textContent  = tech.name;
-        //     div.appendChild(h3);
-        //     let splitText= tech.text.split('\n');
-        //     for (let k2 in splitText ){
-        //         let textFragment = splitText[k2];
-        //         let p = d.createElement("p");
-        //         p.textContent  = textFragment;
-        //         div.appendChild(p);
-        //     }
-            // let prerequisiteGroups = tech.prerequisites.reduce((groups, prerequisite)=>{
-            //         groups[prerequisite] = (groups[prerequisite] || 0) + 1;
-            //         return groups
-            //     },{});
-            // for (let key2 in prerequisiteGroups){
-            //     prerequisiteGroup = prerequisiteGroups[key2];
-            //     let icon = d.createElement("p");
-            //     p.textContent  = string(prerequisiteGroup);
-            //     div.appendChild(p);
-            // }
-        //     list.appendChild(div);
-        // }
+        this.set({ 
+            techs: data.techs.map(t => {
+                let p = t.prerequisites;
+                t.prerequisiteCount = (p.r ||0) + (p.g ||0) + (p.b ||0) + (p.y ||0);
+                return t;
+            })
+        });
     }
 }
 let store = new TechStore({techs:[]});
